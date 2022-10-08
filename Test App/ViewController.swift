078ddd8d6fd9a8b2f36ec467f0c9a94cab92e463
Sergei <sergeyzorko@gmail.com>
2022-10-08 18:08:07 +0300

    first commit

---
+++ Test App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test App
+//
+//  Created by Sergei Zorko on 08.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
